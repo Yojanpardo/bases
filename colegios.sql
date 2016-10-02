7e8edec76144233976805a14d48a5195306a7a33
Yojan Pardo <Yojan.Pardo@gmail.com>
2016-10-02 12:43:01 -0500

    termino base de datos del colegio con las respectivas consultas

--- colegios.sql
+++ colegios.sql
@@ -1,3 +1,4 @@
+### AUTHOR: YOJAN ANTONIO PARDO ZABALA ###
 CREATE DATABASE colegios;
 
 CREATE TABLE colegios(
@@ -11,10 +12,9 @@ VALUES
 	('C.P. Quevedo');
 
 CREATE TABLE aulas(
-	aula_id  VARCHAR(6) NOT NULL,
+	aula_id  VARCHAR(6) PRIMARY KEY NOT NULL,
 	capacidad INTEGER NOT NULL,
-	colegio_id INTEGER REFERENCES colegios(colegio_id),
-	PRIMARY KEY (aula_id,colegio_id)
+	colegio_id INTEGER REFERENCES colegios(colegio_id)
 );
 
 INSERT INTO aulas
@@ -24,13 +24,12 @@ VALUES
 	('2.B01',20,2);
 
 CREATE TABLE profesores(
-	profesor_id VARCHAR(11) NOT NULL,
+	profesor_id VARCHAR(11) PRIMARY KEY NOT NULL,
 	primer_nombre VARCHAR(30) NOT NULL,
 	segundo_nombre VARCHAR(30) DEFAULT '---',
 	primer_apellido VARCHAR(30) NOT NULL,
 	segundo_apellido VARCHAR(30) DEFAULT '---',
-	aula_id VARCHAR(6) REFERENCES aulas(aula_id),
-	PRIMARY KEY (profesor_id,aula_id)
+	aula_id VARCHAR(6) REFERENCES aulas(aula_id)
 );
 
 INSERT INTO profesores
@@ -56,12 +55,12 @@ VALUES
 	('Inglés','2');
 
 CREATE TABLE profesores_asignaturas(
-	asignatura_id VARCHAR(40) REFERENCES asignaturas(asignatura_id),
+	asignatura_id INTEGER REFERENCES asignaturas(asignatura_id),
 	profesor_id VARCHAR(11) REFERENCES profesores(profesor_id),
 	PRIMARY KEY (asignatura_id,profesor_id)
 );
 
-INSERT INTO profesores_asignaturas
+INSERT INTO profesores_asignaturas (profesor_id,asignatura_id)
 VALUES
 	('10',1),
 	('10',2),
@@ -89,12 +88,15 @@ CREATE TABLE libros(
 	libro_id SERIAL NOT NULL,
 	nombre VARCHAR (45) NOT NULL,
 	editorial_id INTEGER REFERENCES editoriales(editorial_id),
-	PRIMARY KEY (libro_id,editorial_id)
+	PRIMARY KEY (libro_id)
 );
 
 INSERT INTO libros (nombre,editorial_id)
 VALUES
 	('APRENEDER Y ENSEÑAR EN EDUCACIÓN INFANTIL',1),
+	('PREESCOLAR RUBIO. N6',2),
+	('EDUCACIÓN INFANTIL. N9',1),
+	('SABER EDUCAR: GUIA PARA PADRES Y PROFESORES',4);
 
 
 CREATE TABLE profesores_libros(
@@ -103,3 +105,33 @@ CREATE TABLE profesores_libros(
 	fecha_prestamo DATE NOT NULL,
 	PRIMARY KEY (profesor_id,libro_id,fecha_prestamo) 
 );
+
+INSERT INTO profesores_libros
+VALUES
+	('10',1,'2016/02/28'),
+	('10',2,'2016/03/01'),
+	('11',3,'2016/02/28'),
+	('11',1,'2016/03/01'),
+	('12',1,'2016/02/28'),
+	('12',4,'2016/03/28'),
+	('13',4,'2016/02/28'),
+	('13',1,'2016/03/28');
+
+
+###SECCIÓN DE CONSULTAS###
+
+SELECT aula_id,capacidad,colegios.nombre
+FROM aulas,colegios
+WHERE colegios.colegio_id=aulas.colegio_id;
+
+SELECT profesores.primer_nombre,profesores.primer_apellido,colegios.nombre
+FROM profesores,colegios,aulas
+WHERE colegios.colegio_id=2 AND colegios.colegio_id=aulas.colegio_id AND profesores.aula_id=aulas.aula_id;
+
+SELECT profesores.primer_nombre, asignaturas.nombre
+FROM profesores, asignaturas, profesores_asignaturas
+WHERE profesores_asignaturas.profesor_id='10' AND profesores_asignaturas.asignatura_id=asignaturas.asignatura_id AND profesores_asignaturas.profesor_id=profesores.profesor_id; 
+
+SELECT profesores.primer_nombre, profesores.primer_apellido
+FROM profesores
+WHERE primer_nombre LIKE 'A%';
